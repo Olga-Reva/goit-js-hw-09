4ef6df0f9db3da19a76e469f650d73af11db3e47
Reva Olga <Reva.Olga.Vladimirovna@gmail.com>
2025-02-02 02:19:08 +0200

    updating

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -14,6 +14,24 @@ EventForm.addEventListener('input', event => {
   }
 });
 
+export const save = (key, data) => {
+  try {
+    localStorage.setItem(key, JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const load = key => {
+  try {
+    const loadDataFromLs = localStorage.getItem(key);
+
+    return loadDataFromLs === null ? undefined : JSON.parse(loadDataFromLs);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 window.addEventListener('load', () => {
   const savedData = localStorage.getItem('feedback-form-state');
   if (savedData) {
